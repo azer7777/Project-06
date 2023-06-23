b5e074f7f0971d7e2976c4388108cfaa50bc0211
azer7777 <azertyuio123a77@gmail.com>
2023-06-23 15:41:24 +0200

    bestmovie

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const reponse = await fetch("pieces-autos.json");
-const movies = await reponse.json();
+
+async function getMovies(endpoint){
+    const reponse = await fetch("http://localhost:8000/api/v1/titles/" + endpoint);
+    const movies = await reponse.json();
+    return movies
+}
+const bestMovie = ((getMovies("?sort_by=-votes,-imdb_score"))[0])["image_url"];
+const imageBestMovie = document.createElement("img");
+imageBestMovie.src = bestMovie.image;
+const sectionMovies = document.querySelector(".image1");
+
+sectionMovies.appendChild(imageBestMovie);
