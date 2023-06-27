ab31371b603341a6b77ae72be1b07303dc86514c
azer7777 <azertyuio123a77@gmail.com>
2023-06-27 18:07:46 +0200

    pending

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 
-async function getMovies(endpoint){
-    const reponse = await fetch(endpoint);
-    const movies = await reponse.json();
-    return movies
+async function getData(anyUrl){
+    let reponse = await fetch(anyUrl);
+    let data = await reponse.json();
+    return data;
 }
-const bestMovie = getMovies("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg");
 
-const imageBestMovie = document.createElement("img");
 
-imageBestMovie.src = bestMovie.image;
-const sectionMovies = document.querySelector(".image1");
 
-sectionMovies.appendChild(imageBestMovie);
+function getBestMovieUrl(urlSortedMovie){
+    let ratedSortedMovies = getData(urlSortedMovie);
+    let bestMovieUrl = ratedSortedMovies.url;
+    let bestMovie = getData(bestMovieUrl);
+    let bestMovieImageUrl = bestMovie.url;
+    document.getElementById("bestmovie_image").innerHTML = "<img src=" + bestMovieImageUrl + "alt='image best movie ' height='300' width='100%'/>";
+    
+}
+
+getBestMovieUrl("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score");
+
+
+ 
+
+
+
+
+
+
+
+
+
