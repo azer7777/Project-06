c290895306428f8a34651d42919235615f461f10
azer7777 <azertyuio123a77@gmail.com>
2023-06-28 02:29:11 +0200

    best rated movies

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 
-async function getData(anyUrl){
+async function getData(anyUrl, loadData){
     let reponse = await fetch(anyUrl);
     let data = await reponse.json();
-    return data;
+    loadData(data);
 }
+function getBestMovieUrl(data){
 
-
-
-function getBestMovieUrl(urlSortedMovie){
-    let ratedSortedMovies = getData(urlSortedMovie);
-    let bestMovieUrl = ratedSortedMovies.url;
-    let bestMovie = getData(bestMovieUrl);
-    let bestMovieImageUrl = bestMovie.url;
-    document.getElementById("bestmovie_image").innerHTML = "<img src=" + bestMovieImageUrl + "alt='image best movie ' height='300' width='100%'/>";
+    let bestMovieUrl = data.results[0].url;
+    getData(bestMovieUrl, loadBestMovieInfo);
     
 }
-
-getBestMovieUrl("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score");
-
+function loadBestMovieInfo(bestMovieUrl){
+    document.getElementById("bestmovie_image").innerHTML = "<img src=" + bestMovieUrl.image_url + "alt='image best movie ' height='500' width='100%'/>";
+    document.getElementById("title-one").innerHTML = bestMovieUrl.title;
+    document.getElementById("description").innerHTML = bestMovieUrl.description;
+}
+let ratingSortedMovieUrl = "http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score"
+getData(ratingSortedMovieUrl, getBestMovieUrl);
 
  
 
