65e744888357244bb1c8dd9e786739e6cb6e552e
azer7777 <azertyuio123a77@gmail.com>
2023-06-30 00:11:59 +0200

    slideshow

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-async function getData(anyUrl, loadData, loadMovieInfo){
+async function getData(anyUrl, loadData, loadMovieInfo, index){
     let response = await fetch(anyUrl);
     let data = await response.json();
-    loadData(data, loadMovieInfo);
+    loadData(data, loadMovieInfo, index);
 }
-function getBestMovieUrl(data, loadMovieInfo){
-    let bestMovieUrl = data.results[0].url;
+function getBestMovieUrl(data, loadMovieInfo, index){
+    let bestMovieUrl = data.results[index].url;
     getData(bestMovieUrl, loadMovieInfo);   
 }
 function loadBestMovieInfo(dataMovie){
@@ -13,25 +13,33 @@ function loadBestMovieInfo(dataMovie){
     document.getElementById("titleBestMovie").innerHTML = dataMovie.title;
     document.getElementById("description").innerHTML = dataMovie.description;
 }
-let ratingSortedMovieUrl = "http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score"
-getData(ratingSortedMovieUrl, getBestMovieUrl, loadBestMovieInfo);
-getData(ratingSortedMovieUrl, getBestMovieUrl, loadInfoToModal);
+let ratingSortedMoviesUrl = "http://localhost:8000/api/v1/titles/?sort_by=-votes,-imdb_score"
+let familyMoviesUrl = "http://localhost:8000/api/v1/titles/?sort_by=-votes,-imdb_score&genre=animation"
+getData(ratingSortedMoviesUrl, getBestMovieUrl, loadBestMovieInfo, 0);
+getData(ratingSortedMoviesUrl, getBestMovieUrl, loadInfoToModal, 0);
+for (let i=1; i<7; i++){
+    getData(ratingSortedMoviesUrl, getBestMovieUrl, loadMoviesImg, i);
+}
+
 function loadInfoToModal(dataMovie){
     document.getElementById("imageMovie").innerHTML = "<img src=" + dataMovie.image_url + "alt='image best movie '/>";
     document.getElementById("title").innerHTML = dataMovie.title;
-    document.getElementById("genres").innerHTML = "genres : " + dataMovie.genres;   
-    document.getElementById("datePublished").innerHTML = "datePublished : " + dataMovie.date_published;
-    document.getElementById("rated").innerHTML = "rated : " + dataMovie.rated;
-    document.getElementById("imdbScore").innerHTML = "imdbScore : " + dataMovie.imdb_score;
-    document.getElementById("directors").innerHTML = "directors : " + dataMovie.directors;
-    document.getElementById("actors").innerHTML = "actors : " + dataMovie.actors;
-    document.getElementById("duration").innerHTML = "duration : " + dataMovie.duration + ' min';
-    document.getElementById("countries").innerHTML = "countries : " + dataMovie.countries;
-    document.getElementById("worldwideGrossIncome").innerHTML = "worldwideGrossIncome : " + dataMovie.worldwide_gross_income + ' $';
-    document.getElementById("longDescription").innerHTML = "longDescription : " + dataMovie.long_description;
+    document.getElementById("genres").innerHTML = "Genres : " + dataMovie.genres;   
+    document.getElementById("datePublished").innerHTML = "Date published : " + dataMovie.date_published;
+    document.getElementById("rated").innerHTML = "Rated : " + dataMovie.rated;
+    document.getElementById("imdbScore").innerHTML = "Imdb score : " + dataMovie.imdb_score;
+    document.getElementById("directors").innerHTML = "Directors : " + dataMovie.directors;
+    document.getElementById("actors").innerHTML = "Actors : " + dataMovie.actors;
+    document.getElementById("duration").innerHTML = "Duration : " + dataMovie.duration + ' min';
+    document.getElementById("countries").innerHTML = "Countries : " + dataMovie.countries;
+    document.getElementById("worldwideGrossIncome").innerHTML = "Worldwide gross income : " + dataMovie.worldwide_gross_income + ' $';
+    document.getElementById("longDescription").innerHTML = "Long description : " + dataMovie.long_description;
 }
 
-
+function loadMoviesImg(dataMovie){
+    document.getElementById("rated_movie_img_" + n).innerHTML = "<img src=" + dataMovie.image_url + "alt='image movie '/>";
+    
+}
 
 
 
